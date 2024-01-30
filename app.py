fdf97bd936b7162d1c3671bdf5991d95014f8884
Saketh Sripada <sakeats@gmail.com>
2024-01-29 23:03:39 -0600

    added scheduled upload date and time functionality

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, jsonify, render_template
 from instagrapi import Client
 import os
+import datetime
+import sched
+import time
+import threading
+
+scheduler = sched.scheduler(time.time, time.sleep)
 
 app = Flask(__name__)
 
@@ -20,19 +26,42 @@ def ig_login(username, password):
     return client
 
 
+def sched_item(delay, function, *args):
+    def exec_task():
+        scheduler.enter(delay, 1, function, args)
+        scheduler.run()
+
+    threading.Thread(target=exec_task).start()
+
+
+def exec_post(username, password, file_path, caption):
+    client = ig_login(username, password)
+    client.photo_upload(file_path, caption)
+
+
 @app.route("/post", methods=["POST"])
 def post_image():
     username = request.form['username']
     password = request.form['password']
     caption = request.form['caption']
     photo = request.files["photo"]
+    scheduled_time = request.form.get("schedule_time")
 
     file_path = os.path.join(app.config["UPLOAD_FOLDER"], photo.filename)
     photo.save(file_path)
-    client = ig_login(username, password)
-    client.photo_upload(file_path, caption)
 
-    return "Image Posted!"
+    if scheduled_time:
+        scheduled_time = datetime.datetime.strptime(scheduled_time, "%Y-%m-%dT%H:%M")
+        post_delay = (scheduled_time - datetime.datetime.now()).total_seconds()
+        if post_delay > 0:
+            sched_item(post_delay, exec_post, username, password, file_path, caption)
+            return "Successfully Scheduled."
+        else:
+            exec_post(username, password, file_path, caption)
+            return "Scheduled time has passed. Image Posting Now."
+    else:
+        exec_post(username, password, file_path, caption)
+        return "Successfully Posted"
 
 
 @app.route("/like", methods=["POST"])
