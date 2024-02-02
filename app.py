4682fa0005e31ec3f7cdf382a2dac2666bdd9bb6
Saketh Sripada <saketh.sripada@gmail.com>
2024-02-02 12:46:33 -0600

    added password requirements

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError, Regexp
 from instance.secret import SECRET_KEY
 import uuid
 import os
@@ -43,8 +43,15 @@ if __name__ == '__main__':
 
 class RegisterForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
-    password = PasswordField("Password", validators=[DataRequired()])
-    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
+    password = PasswordField("Password", validators=[
+        DataRequired(),
+        Regexp(r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$',
+               message="Your password must be at least 8 characters long and include a letter, number, and special "
+                       "character.")
+    ])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(),
+                                                                     EqualTo("password",
+                                                                             message="Passwords Must Match")])
     submit = SubmitField("Sign Up")
 
     def validate_username(self, username):
